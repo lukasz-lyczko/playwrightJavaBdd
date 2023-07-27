53f2f2d45640c8c9f7c46e7473916bfcab572f87
marle <marlene79@o2.pl>
2023-07-27 21:03:43 +0200

    2 commit

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -9,10 +9,14 @@ import modules.Header;
 public class BasePage extends BaseModule {
     public Header header;
     public Locator alert;
+    public Locator button;
+    public Locator shoppingCartButton;
 
     public BasePage(Page page) {
         super(page);
         this.header = new Header(page);
         this.alert = page.getByRole(AriaRole.ALERT);
+        this.button = page.locator("button");
+        this.shoppingCartButton = page.locator(".showcart");
     }
 }
